a5c128fc3efcb2dcaca6d240c2b8f892a9474cba
jessicaagren <jessicaagren@hotmail.com>
2025-03-05 15:47:46 +0100

    Add CreateHearts and connect links to different pages

--- src/components/FourthPageComponent/FourthPageComponent.tsx
+++ src/components/FourthPageComponent/FourthPageComponent.tsx
@@ -3,10 +3,46 @@ import './FourthPageComponent.scss';
 function FourthPageComponent({ onButtonClick }: { onButtonClick: () => void }) {
   return (
     <div className='FourthPageComponent'>
-      <h1>Stephanie är en prutt! 💩</h1>
-      <button className='yes-btn' onClick={onButtonClick}>
-        Gå tillbaka till första sidan
-      </button>
+      <span id='arrow-span'>
+        <button onClick={onButtonClick}>
+          <img
+            src='/media/pixel-arrow-left.png'
+            className='arrow-left'
+            height='100px'
+            width='100px'
+            alt='Back Arrow'
+          />
+        </button>
+      </span>
+      <div className='container'>
+        <h1>Hoppas du mår bra!</h1>
+        <span id='text-emoji-wrapper'>
+          <p>Lyssna på den här och tänk på oss</p>
+          <img
+            id='flirty-emoji'
+            src='/media/flirty-emoji.png'
+            alt='Flirty Emoji'
+          />
+        </span>
+        <div className='player-container'>
+          <iframe
+            style={{ borderRadius: '12px' }}
+            src='https://open.spotify.com/embed/playlist/5JevSFPTByDXDZzpVTmKYs?utm_source=generator'
+            width='100%'
+            height='352'
+            frameBorder='0'
+            allowFullScreen
+            allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
+            loading='lazy'></iframe>
+        </div>
+        <h2>Siri & Jessica</h2>
+        <img
+          src='/media/pixel-heart.png'
+          height='40px'
+          width='40px'
+          alt='Heart Icon'
+        />
+      </div>
     </div>
   );
 }
--- src/components/SecondPageComponent/SecondPageComponent.tsx
+++ src/components/SecondPageComponent/SecondPageComponent.tsx
@@ -1,6 +1,18 @@
 import './SecondPageComponent.scss';
 
 function SecondPageComponent({ onButtonClick }: { onButtonClick: () => void }) {
+  const triggerExplosion = () => {
+    const noButton = document.getElementById('noBtn') as HTMLButtonElement;
+
+    if (noButton) {
+      noButton.classList.add('explode');
+
+      setTimeout(() => {
+        noButton.remove();
+      }, 500);
+    }
+  };
+
   return (
     <div className='SecondPageComponent'>
       <span id='arrow-span'>
@@ -10,6 +22,7 @@ function SecondPageComponent({ onButtonClick }: { onButtonClick: () => void }) {
             className='arrow-left'
             height='100px'
             width='100px'
+            alt='Back Arrow'
           />
         </a>
       </span>
@@ -25,12 +38,12 @@ function SecondPageComponent({ onButtonClick }: { onButtonClick: () => void }) {
         <h1 id='heading-text'>Saknar du oss också?</h1>
         <div className='buttons-container'>
           <div className='button-wrapper'>
-            <button id='yesBtn' className='yes-btn'>
+            <button id='yesBtn' className='yes-btn' onClick={onButtonClick}>
               Ja
             </button>
           </div>
           <div className='button-wrapper'>
-            <button id='noBtn' className='no-btn'>
+            <button id='noBtn' className='no-btn' onClick={triggerExplosion}>
               Nej
             </button>
           </div>
