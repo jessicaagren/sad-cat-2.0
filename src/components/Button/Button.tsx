7716a30fe6c02f25413414056c9246ea4e1f4218
jessicaagren <jessicaagren@hotmail.com>
2025-03-07 11:59:14 +0100

    Add Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import './Button.scss';
+
+type ArrowProps = {
+  title: string;
+  id?: string;
+  className?: string;
+  onButtonClick: () => void;
+};
+
+function Button({ title, id, className, onButtonClick }: ArrowProps) {
+  return (
+    <button id={id} className={`Button ${className}`} onClick={onButtonClick}>
+      {title}
+    </button>
+  );
+}
+
+export default Button;
