39e2d7a5d9816b212aa4a18826e5810ba3836e5d
jessicaagren <jessicaagren@hotmail.com>
2025-03-07 13:28:25 +0100

    Remove Main and move to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import './App.scss';
-import Main from './components/Main/Main';
+
+import { useState } from 'react';
+import Arrow from './components/Arrow/Arrow';
+import FirstPageComponent from './components/FirstPageComponent/FirstPageComponent';
+import SecondPageComponent from './components/SecondPageComponent/SecondPageComponent';
+import ThirdPageComponent from './components/ThirdPageComponent/ThirdPageComponent';
+import FourthPageComponent from './components/FourthPageComponent/FourthPageComponent';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+
   return (
-    <>
-      <Main />
-    </>
+    <div>
+      <span id='arrow-span'>
+        {currentPage > 1 && (
+          <Arrow onButtonClick={() => setCurrentPage(currentPage - 1)} />
+        )}
+      </span>
+
+      {currentPage === 1 && (
+        <FirstPageComponent onButtonClick={() => setCurrentPage(2)} />
+      )}
+      {currentPage === 2 && (
+        <SecondPageComponent onButtonClick={() => setCurrentPage(3)} />
+      )}
+      {currentPage === 3 && (
+        <ThirdPageComponent onButtonClick={() => setCurrentPage(4)} />
+      )}
+      {currentPage === 4 && (
+        <FourthPageComponent onButtonClick={() => setCurrentPage(3)} />
+      )}
+    </div>
   );
 }
 
