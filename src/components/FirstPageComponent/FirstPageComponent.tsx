e30585ed92f5b43188dbcce4273c9296d1cf20e7
jessicaagren <jessicaagren@hotmail.com>
2025-03-11 14:28:51 +0100

    Update todo

--- src/components/FirstPageComponent/FirstPageComponent.tsx
+++ src/components/FirstPageComponent/FirstPageComponent.tsx
@@ -5,10 +5,9 @@ function FirstPageComponent({ onButtonClick }: { onButtonClick: () => void }) {
   return (
     <div className='FirstPageComponent'>
       <h1>Stephanie!!!!!!! We miss you!!!!</h1>
-      <p>Fixa arrow</p>
-      <p>Gör components av dem</p>
+      <p>Fixa nej-knapp (hoppa)</p>
       <p>Byta namn på saker?</p>
-      <p>Fixa knapplänkarna (arrows)</p>
+      <p>Centrering av knappar</p>
       <Button
         title='Klicka här'
         className='yes-button'
