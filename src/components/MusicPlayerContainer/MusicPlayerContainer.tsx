a403154131b281f5d76cb050d4cd9a4ed922745c
jessicaagren <jessicaagren@hotmail.com>
2025-03-14 10:32:45 +0100

    Add styles to music player (iframe)

--- src/components/MusicPlayerContainer/MusicPlayerContainer.tsx
+++ src/components/MusicPlayerContainer/MusicPlayerContainer.tsx
@@ -1,12 +1,10 @@
+import './MusicPlayerContainer.scss';
+
 function MusicPlayerContainer() {
   return (
     <div className='player-container'>
       <iframe
-        style={{ borderRadius: '12px' }}
         src='https://open.spotify.com/embed/playlist/5JevSFPTByDXDZzpVTmKYs?utm_source=generator'
-        width='100%'
-        height='352'
-        frameBorder='0'
         allowFullScreen
         allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
         loading='lazy'></iframe>
