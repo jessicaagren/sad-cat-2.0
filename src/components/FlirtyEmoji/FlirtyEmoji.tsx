5b9d4572ae3fc8d6c58d4c80cf46d781f450d2bb
jessicaagren <jessicaagren@hotmail.com>
2025-03-14 10:46:48 +0100

    Add FlirtyEmoji component

---
+++ src/components/FlirtyEmoji/FlirtyEmoji.tsx
@@ -0,0 +1,11 @@
+import './FlirtyEmoji.scss';
+
+function FlirtyEmoji() {
+  return (
+    <>
+      <img id='flirty-emoji' src='/media/flirty-emoji.png' alt='Flirty Emoji' />
+    </>
+  );
+}
+
+export default FlirtyEmoji;
