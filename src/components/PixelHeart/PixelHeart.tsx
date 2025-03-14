33dd8bf2e753e4d41372c86d17911b404586e43c
jessicaagren <jessicaagren@hotmail.com>
2025-03-14 10:46:57 +0100

    Add PixelHeart component

---
+++ src/components/PixelHeart/PixelHeart.tsx
@@ -0,0 +1,11 @@
+import './PixelHeart.scss';
+
+function PixelHeart() {
+  return (
+    <>
+      <img id='PixelHeart' src='/media/pixel-heart.png' alt='Heart Icon' />
+    </>
+  );
+}
+
+export default PixelHeart;
